05eb9fe289ace45737552f2ca2eaf8ec369af374
John Fitzpatrick <jfitz@computer.org>
2022-01-28 16:54:59 -0800

    Add tests for CP/M.

--- test/bin/run_stdin_tests.sh
+++ test/bin/run_stdin_tests.sh
@@ -16,6 +16,7 @@ mkdir "$TESTBED"
 echo Running all tests...
 ECODE=0
 
+# HDOS
 # stats
 test/bin/run_stdin.sh test tests EmptyHDOSImages 1s40t-stats test/EmptyHDOSImages/data/1s40t.h8d test/bin/stdin_hdos_stats.txt
 test/bin/run_stdin.sh test tests EmptyHDOSImages 1s80t-stats test/EmptyHDOSImages/data/1s80t.h8d test/bin/stdin_hdos_stats.txt
@@ -27,3 +28,19 @@ test/bin/run_stdin.sh test tests EmptyHDOSImages 1s80t-cat test/EmptyHDOSImages/
 # dir
 test/bin/run_stdin.sh test tests EmptyHDOSImages 1s40t-dir test/EmptyHDOSImages/data/1s40t.h8d test/bin/stdin_hdos_dir.txt
 test/bin/run_stdin.sh test tests EmptyHDOSImages 1s80t-dir test/EmptyHDOSImages/data/1s80t.h8d test/bin/stdin_hdos_dir.txt
+
+# CP/M
+# stats
+test/bin/run_stdin.sh test tests CPM_Apps c80_1-stats test/CPM_Apps/data/C80CPM1.h8d test/bin/stdin_cpm_stats.txt
+test/bin/run_stdin.sh test tests CPM_Apps c80_2-stats test/CPM_Apps/data/C80CPM2.h8d test/bin/stdin_cpm_stats.txt
+test/bin/run_stdin.sh test tests CPM_Apps c80_3-stats test/CPM_Apps/data/C80CPM3.h8d test/bin/stdin_cpm_stats.txt
+
+# cat
+test/bin/run_stdin.sh test tests CPM_Apps c80_1-cat test/CPM_Apps/data/C80CPM1.h8d test/bin/stdin_cpm_cat.txt
+test/bin/run_stdin.sh test tests CPM_Apps c80_2-cat test/CPM_Apps/data/C80CPM2.h8d test/bin/stdin_cpm_cat.txt
+test/bin/run_stdin.sh test tests CPM_Apps c80_3-cat test/CPM_Apps/data/C80CPM3.h8d test/bin/stdin_cpm_cat.txt
+
+# dir
+test/bin/run_stdin.sh test tests CPM_Apps c80_1-dir test/CPM_Apps/data/C80CPM1.h8d test/bin/stdin_cpm_dir.txt
+test/bin/run_stdin.sh test tests CPM_Apps c80_2-dir test/CPM_Apps/data/C80CPM2.h8d test/bin/stdin_cpm_dir.txt
+test/bin/run_stdin.sh test tests CPM_Apps c80_3-dir test/CPM_Apps/data/C80CPM3.h8d test/bin/stdin_cpm_dir.txt
